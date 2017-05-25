e3cb1c62dac85fcaea9f046880146a01e105c882
Sunny <sunny7246161@hotmail.com>
2017-05-25 19:20:56 +0800

    first commit

---
+++ Assets/backpackController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backpackController : MonoBehaviour {
+
+	public float speed = 30f;
+	public bool isColli = false;
+
+	private float x = 5f;
+
+	// Use this for initialization
+	void Start () {
+		//GetComponent<Rigidbody2D> ().AddForce(transform.right * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().AddForce(transform.right * speed);
+		if (Input.GetMouseButton(0)){
+			//this.
+			Debug.Log("keep");
+		}
+	}
+
+	//void OnCollisionEnter(Collision other)
+	//{	
+	//	Debug.Log ("Hi");
+	//}
+
+
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.tag == "enemy") 
+		{
+			target.gameObject.SetActive (false);
+			this.gameObject.SetActive (false);
+		}
+	
+	}
+
+
+}
